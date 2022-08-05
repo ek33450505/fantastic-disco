9e4f64506853cb8cd9be1c81e37bfa6368079146
Ed Kubiak <ek33450505@gmail.com>
2022-08-04 21:27:23 -0400

    changed js and css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,29 @@
-function currentTime() {
-    let date = new Date(); 
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
-    let session = "AM";
-  
-    if(hh == 0){
-        hh = 12;
+function currentTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
     }
-    if(hh > 12){
-        hh = hh - 12;
+    
+    if(h > 12){
+        h = h - 12;
         session = "PM";
-     }
-  
-     hh = (hh < 10) ? "0" + hh : hh;
-     mm = (mm < 10) ? "0" + mm : mm;
-     ss = (ss < 10) ? "0" + ss : ss;
-      
-     let time = hh + ":" + mm + ":" + ss + " " + session;
-  
-    document.getElementById("clock").innerText = time; 
-    let t = setTimeout(function(){ currentTime() }, 1000);
-  }
-  currentTime();
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("ClockDisplay").innerText = time;
+    document.getElementById("ClockDisplay").textContent = time;
+    
+    setTimeout(currentTime, 1000);
+    
+}
+
+currentTime();
